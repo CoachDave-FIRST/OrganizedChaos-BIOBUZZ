92176dc06dc25b5de8829223332a07d2c04774a3
RISE-r0b0t1cs <dave.anderson@roboticinnovation.org>
2026-09-15 13:16:53 -0400

    Update MecanumDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MecanumDrive.java
@@ -1,4 +1,84 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
-public class MecanumDrive {
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class MecanumDrive extends OpMode {
+    public DcMotor frontRight;
+    public DcMotor backRight;
+    public DcMotor frontLeft;
+    public DcMotor backLeft;
+    public IMU imu;
+
+    @Override
+    public void init() {
+
+        frontRight = hardwareMap.get(DcMotor.class,"rightFrontDriveMotor");
+        frontLeft = hardwareMap.get(DcMotor.class, "leftFrontDriveMotor");
+        backLeft = hardwareMap.get(DcMotor.class, "leftRearDriveMotor");
+        backRight = hardwareMap.get(DcMotor.class, "rightRearDriveMotor");
+
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+
+       imu = hardwareMap.get(IMU.class, "imu");
+
+        RevHubOrientationOnRobot RevOrientation = new RevHubOrientationOnRobot
+                (RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                        RevHubOrientationOnRobot.UsbFacingDirection.RIGHT);
+
+        imu.initialize(new IMU.Parameters(RevOrientation));
+    }
+
+    @Override
+    public void loop() {
+
+        //now i know where i am thingy
+        if (gamepad1.a){
+            imu.resetYaw();
+        }
+
+        //go to the place thingy
+        double forward = gamepad1.left_stick_y;
+        double right = -gamepad1.left_stick_x;
+        double rotate = -gamepad1.right_stick_x;
+
+        double theta = Math.atan2(forward, right);
+        double r = Math.hypot(right, forward);
+
+        theta = AngleUnit.normalizeRadians(theta -
+                imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+
+        double newForward = r * Math.sin(theta);
+        double newRight = r * Math.cos(theta);
+
+        double frontLeftPower = newForward + newRight + rotate;
+        double frontRightPower = newForward - newRight - rotate;
+        double backRightPower = newForward + newRight - rotate;
+        double backLeftPower = newForward - newRight + rotate;
+
+        double maxPower = 1.0;
+        double maxSpeed = 1.0;
+
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
+
+        frontLeft.setPower(maxSpeed * (frontLeftPower / maxPower));
+        frontRight.setPower(maxSpeed * (frontRightPower / maxPower));
+        backLeft.setPower(maxSpeed * (backLeftPower / maxPower));
+        backRight.setPower(maxSpeed * (backRightPower / maxPower));
+
+    }
 }
